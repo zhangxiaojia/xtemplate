a967458e600fa2d38048df199b97d907d554c13c
yiminghe <yiminghe@gmail.com>
2015-11-19 19:40:00 +0800

    Updates

--- examples/simple.js
+++ examples/simple.js
@@ -1,4 +1,24 @@
-var XTemplate = require('../');
-console.log(new XTemplate('Hello {{world}}!').render({
-    world: 'world'
-}));
+webpackJsonp([2],{
+
+/***/ 0:
+/***/ function(module, exports, __webpack_require__) {
+
+	module.exports = __webpack_require__(326);
+
+
+/***/ },
+
+/***/ 326:
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+	
+	var XTemplate = __webpack_require__(160);
+	console.log(new XTemplate('Hello {{world}}!').render({
+	    world: 'world'
+	}));
+
+/***/ }
+
+});
+//# sourceMappingURL=simple.js.map
